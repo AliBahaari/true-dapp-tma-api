78c85c579bd7337aaa542292e33ae658111dbed0
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2025-02-18 05:04:09 +0330

    fix(): remove comments

--- src/file/service/sftp.service.ts
+++ src/file/service/sftp.service.ts
@@ -14,15 +14,13 @@ export class SftpService {
 
     async uploadFile(localFilePath: string, remoteFilePath: string): Promise<void> {
         try {
-            // Connect to the backup server
             await this.sftp.connect({
-                host: 'ec2-40-172-185-159.me-central-1.compute.amazonaws.com', // Backup server IP or hostname
-                port: 22, // SSH port
-                username: 'ubuntu', // SSH username
-                privateKey: fs.readFileSync('/app/keys/BkKey.pem'), // Path to your private key
+                host: 'ec2-40-172-185-159.me-central-1.compute.amazonaws.com',
+                port: 22,
+                username: 'ubuntu',
+                privateKey: fs.readFileSync('/app/keys/BkKey.pem'),
             });
 
-            // Upload the file
             await this.sftp.put(localFilePath, remoteFilePath);
 
             console.log(`File uploaded successfully to ${remoteFilePath}`);
@@ -30,7 +28,6 @@ export class SftpService {
             console.error('Error uploading file:', error);
             throw new Error('Failed to upload file');
         } finally {
-            // Close the SFTP connection
             await this.sftp.end();
         }
     }
